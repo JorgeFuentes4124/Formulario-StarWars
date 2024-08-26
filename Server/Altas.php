f1d8e028b81a83f373d4d30a6e5201c0b66dfe64
NicoDelgadoHUB <ncklsdelgado@gmail.com>
2024-08-26 17:20:12 -0300

    Update Altas.php

--- Server/Altas.php
+++ Server/Altas.php
@@ -44,6 +44,7 @@ $result = insertar($usu, $nom, $ape, $fe, $cla, $foto, $fotoTamanio);
 if(strlen($result) > 5 ) {
     echo '<div class="Rcontainer">
     <div class="Rbox">
+	<h1>hola</h1>
         <h2 class="Rtitulo">Esta es una respuesta del servidor</h2>
         <h3 class="Rcuerpo">' . $result . '</h3>
         <a href="../from/menu.php" class="cerrar">Cerrar</a>
