71adce1af4d2339b52f50c933a27f85bd0245f04
JorgeFuentes <jorgee2007fc@gmail.com>
2024-08-26 17:29:48 -0300

    baaa

--- Server/Altas.php
+++ Server/Altas.php
@@ -1,47 +1,28 @@
-<<<<<<< HEAD
-<div class="form">
-	<h3 class="bajaTitulo" style="text-align: center;">FORMULARIO DE BAJA</h3>
-	<form class="bajaInput" action="../server/bajas.php" method="POST">
-		<div class="grupoInputs">
-			<label for="userName">Seleccione un nombre de usuario:</label>
-			<div class="container-select">
-				<?php
-				include("../modelo/usuario.php");
-				echo getUsuarioUsersNames();
-				?>
-				<div id="modal" class=""></div>
-			</div>
-		</div>
-
-		<div class="btn-baja">
-			<input type="submit" value="Eliminar"><br>
-		</div>
-	</form>
-</div>
-=======
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Server altas</title>
+    <link rel="stylesheet" href="../css/respuesta-alta.css">
 </head>
 
 <?php
 
 include("../modelo/usuario.php");
 
-$usu = $_POST['usuario'];
-$nom = $_POST['nombre'];
-$ape = $_POST['apellido'];
-$fe = $_POST['fecha'];
+$use = $_POST['usuario'];
 $cla = $_POST['clave'];
+$ape = $_POST['apellido'];
+$nom = $_POST['nombre'];
+$fe = $_POST['fecha'];
 $foto = $_FILES["foto"]["tmp_name"];
-$fotoTamanio = $_FILES["foto"]["size"]
+$fotoTamanio = $_FILES["foto"]["size"];
 
-$result = insertar($usu, $nom, $ape, $fe, $cla, $foto, $fotoTamanio);
+$result = insertar($use, $cla, $ape, $nom, $fe, $foto, $fotoTamanio);
 
-if(strlen($result) > 5 ) {
+if (strlen($result) > 5) {
     echo '<div class="Rcontainer">
     <div class="Rbox">
         <h2 class="Rtitulo">Esta es una respuesta del servidor</h2>
@@ -53,4 +34,4 @@ if(strlen($result) > 5 ) {
     return include("../from/respuestas-server/respuesta-alta.php");
 }
 ?>
->>>>>>> c1bf82fb827f39584dfe28ad498e578ca509edf3
+
--- Server/Bajas.php
+++ Server/Bajas.php
@@ -1,30 +1,31 @@
-<<<<<<< HEAD
-<div class="form">
-	<h3 class="bajaTitulo" style="text-align: center;">FORMULARIO DE BAJA</h3>
-	<form class="bajaInput" action="../server/bajas.php" method="POST">
-		<div class="grupoInputs">
-			<label for="userName">Seleccione un nombre de usuario:</label>
-			<div class="container-select">
-				<?php
-				include("../modelo/usuario.php");
-				echo getUsuarioUsersNames();
-				?>
-				<div id="modal" class=""></div>
-			</div>
-		</div>
 
-		<div class="btn-baja">
-			<input type="submit" value="Eliminar"><br>
-		</div>
-	</form>
-</div>
-=======
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Server altas</title>
+	<link rel="stylesheet" href="../css/respuesta-alta.css">
 </head>
 
->>>>>>> c1bf82fb827f39584dfe28ad498e578ca509edf3
+<?php
+
+include("../modelo/usuario.php");
+
+$use = $_POST['userName'];
+
+$result = deleteUser($use);
+
+if ($result) {
+	return include("../from/respuestas-server/respuesta-baja.php");
+} else {
+	echo '<div class="Rcontainer">
+    <div class="Rbox">
+        <h2 class="Rtitulo">Esta es una respuesta del servidor</h2>
+        <h3 class="Rcuerpo">No se pudo eliminar correctamente este registro</h3>
+        <a href="../from/menu.php" class="cerrar">Cerrar</a>
+    </div>
+</div>';
+}
+?>
