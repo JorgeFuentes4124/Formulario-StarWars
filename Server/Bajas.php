887ea440755e13dd48fe1b052c5876f46dc913e4
sancho_panza <ncklsdelgado@gmail.com>
2024-08-28 14:17:58 -0300

    Update Bajas.php

--- Server/Bajas.php
+++ Server/Bajas.php
@@ -1,31 +1,30 @@
+<<<<<<< HEAD
+<div class="form">
+	<h3 class="bajaTitulo" style="text-align: center;">FORMULARIO DE BAJA</h3>
+	<form class="bajaInput" action="../server/bajas.php" method="POST">
+		<div class="grupoInputs">
+			<label for="userName">Seleccione un nombre de usuario:</label>
+			<div class="container-select">
+				<?php
+				include("../modelo/usuario.php");
+				echo getUsuarioUsersNames();
+				?>
+				<div id="modal" class=""></div>
+			</div>
+		</div>
 
+		<div class="btn-baja">
+			<input type="submit" value="Eliminar"><br>
+		</div>
+	</form>
+</div>
+=======
 <!DOCTYPE html>
-<html lang="es">
-
+<html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Server altas</title>
-	<link rel="stylesheet" href="../css/respuesta-alta.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 </head>
 
-<?php
-
-include("../Conexion/Usuario.php");
-
-$use = $_POST['userName'];
-
-$result = deleteUser($use);
-
-if ($result) {
-	return include("../from/respuestas-server/respuesta-baja.php");
-} else {
-	echo '<div class="Rcontainer">
-    <div class="Rbox">
-        <h2 class="Rtitulo">Esta es una respuesta del servidor</h2>
-        <h3 class="Rcuerpo">No se pudo eliminar correctamente este registro</h3>
-        <a href="../from/Menu.php" class="cerrar">Cerrar</a>
-    </div>
-</div>';
-}
-?>
+>>>>>>> c1bf82fb827f39584dfe28ad498e578ca509edf3
