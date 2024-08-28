a3e3bce77d003afc35a84b2c78767088cdce3eeb
sancho_panza <ncklsdelgado@gmail.com>
2024-08-28 14:17:31 -0300

    Update formbajas.php

--- Form/formbajas.php
+++ Form/formbajas.php
@@ -1,12 +1,12 @@
 
 <div class="form">
 	<h3 class="bajaTitulo" style="text-align: center;">FORMULARIO DE BAJA</h3>
-	<form class="bajaInput" action="../Server/Bajas.php" method="POST">
+	<form class="bajaInput" action="../server/bajas.php" method="POST">
 		<div class="grupoInputs">
 			<label for="userName">Seleccione un nombre de usuario:</label>
 			<div class="container-select">
 				<?php
-				include("../Conexion/Usuario.php");
+				include("../modelo/usuario.php");
 				echo getUsuarioUsersNames();
 				?>
 				<div id="modal" class=""></div>
