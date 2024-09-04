20dd226cadb1909c801a60d3ca6eb5e5f1667db5
Evelynuwu3456 <ncklsdelgado@gmail.com>
2024-09-04 14:53:57 -0300

    Update index.php

--- index.php
+++ index.php
@@ -29,6 +29,9 @@
         <div class="star-wars-svg-text">Star Wars</div>
         <h1 class="bienvenida">En un formulario muy lejano, los jedis y siths compartian una fuente de datos para el guardado de datos personales</h1>
     </div>
+
+
+
     <?php
     header("refresh:5;url=Form/Menu.php");
     exit();
