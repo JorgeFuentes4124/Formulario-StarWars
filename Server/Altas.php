0a5b7b977076156dc5e3b52acde9f997ef1acbbe
JorgeFuentes4124 <jorgee2007fc@gmail.com>
2024-09-09 16:29:12 -0300

    Update Altas.php

--- Server/Altas.php
+++ Server/Altas.php
@@ -5,11 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-
+<body>
 <?php
-
 include("../Conexion/Usuario.php");
 
+// Recibir datos del formulario
 $usu = $_POST['usuario'];
 $nom = $_POST['nombre'];
 $ape = $_POST['apellido'];
@@ -18,17 +18,21 @@ $cla = $_POST['clave'];
 $foto = $_FILES["foto"]["tmp_name"];
 $fotoTamanio = $_FILES["foto"]["size"];
 
+// Llamar a la función de inserción
 $result = insertar($usu, $nom, $ape, $fe, $cla, $foto, $fotoTamanio);
 
-if(strlen($result) > 5 ) {
+// Mostrar resultado
+if(strlen($result) > 5) {
     echo '<div class="Rcontainer">
-    <div class="Rbox">
-        <h2 class="Rtitulo">Esta es una respuesta del servidor</h2>
-        <h3 class="Rcuerpo">' . $result . '</h3>
-        <a href="../form/Menu.php" class="cerrar">Cerrar</a>
-    </div>
-</div>';
+        <div class="Rbox">
+            <h2 class="Rtitulo">Esta es una respuesta del servidor</h2>
+            <h3 class="Rcuerpo">' . htmlspecialchars($result, ENT_QUOTES, 'UTF-8') . '</h3>
+            <a href="../form/Menu.php" class="cerrar">Cerrar</a>
+        </div>
+    </div>';
 } else {
-    return include("../Form/Menu.php");
+    include("../Form/Menu.php");
 }
 ?>
+</body>
+</html>
