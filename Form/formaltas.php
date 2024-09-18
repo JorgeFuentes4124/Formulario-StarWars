3d1be1d4cc66442e9f98541f881dda4ba68082cb
Hermann <164916179+HeilKrieg@users.noreply.github.com>
2024-09-17 23:19:57 -0300

    jeje
    blur de clave y campos obligatorios

--- Form/formaltas.php
+++ Form/formaltas.php
@@ -1,30 +1,34 @@
-
 <div class="form">
 	<h3 class="altaTitulo">FORMULARIO DE ALTA</h3>
-	<form class="altaInput" action="../server/altas.php" method="POST" enctype="multipart/form-data">
+	<form class="altaInput" action="../Server/Altas.php" method="POST" enctype="multipart/form-data" onsubmit="return validarFormulario()">
 		<div class="grupoInputs">
 			<img onclick="seleccionarArchivo()" src="../images/fotoPerfil.png" id="imgPerfil" alt="">
-			<input type="file" class="inputFile" name="foto"><br>
+			<input type="file" class="inputFile" name="foto" required><br>
 		</div>
 
 		<div class="grupoInputs">
-			<input type="text" placeholder="usuario" name="usuario"><br>
+			<input type="text" placeholder="usuario" name="usuario" required><br>
 		</div>
 
 		<div class="grupoInputs">
-			<input type="text" placeholder="clave" name="clave"><br>
+			<div style="position: relative;">
+				<input type="password" placeholder="clave" name="clave" id="clave" required>
+				<span id="togglePassword" style="position: absolute; right: 10px; top: 50%; transform: translateY(-50%); cursor: pointer;">
+					<img src="../images/ojo.png" alt="Mostrar" id="iconoOjo">
+				</span>
+			</div><br>
 		</div>
 
 		<div class="grupoInputs">
-			<input type="text" placeholder="Apellido" name="apellido"><br>
+			<input type="text" placeholder="Apellido" name="apellido" required><br>
 		</div>
 
 		<div class="grupoInputs">
-			<input type="text" placeholder="nombre" name="nombre"><br>
+			<input type="text" placeholder="nombre" name="nombre" required><br>
 		</div>
 
 		<div class="grupoInputs">
-			<input type="date" placeholder="fecha" name="fecha"><br>
+			<input type="date" placeholder="fecha" name="fecha" required><br>
 		</div>
 
 		<div class="btn-submit">
@@ -32,3 +36,32 @@
 		</div>
 	</form>
 </div>
+
+<script>
+	
+	const togglePassword = document.getElementById('togglePassword');
+	const passwordField = document.getElementById('clave');
+	const iconoOjo = document.getElementById('iconoOjo');
+
+	togglePassword.addEventListener('click', function () {
+		const type = passwordField.getAttribute('type') === 'password' ? 'text' : 'password';
+		passwordField.setAttribute('type', type);
+		
+		iconoOjo.src = type === 'password' ? '../images/ojo.png' : '../images/ojoCerrado.png';
+	});
+
+	function validarFormulario() {
+		const inputs = document.querySelectorAll('input[required]');
+		let formularioValido = true;
+
+		inputs.forEach(input => {
+			if (!input.value) {
+				alert(`El campo ${input.placeholder} es obligatorio.`);
+				formularioValido = false;
+				return false;
+			}
+		});
+
+		return formularioValido;
+	}
+</script>
