2821939fbe2948aaec9dc6aa5d23487215c3e4c4
NicoDelgadoHUB <ncklsdelgado@gmail.com>
2024-10-07 16:27:48 -0300

    Update Menu.php

--- Form/Menu.php
+++ Form/Menu.php
@@ -15,6 +15,7 @@ header("Expires: Sat, 1 Jul 2000 05:00:00 GMT"); // Fecha en el pasado
 	<meta http-equiv="Cache-Control" content="no-cache, mustrevalidate">
 	<meta http-equiv="Pragma" content="no-cache">
 	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link href="https://fonts.cdnfonts.com/css/star-wars" rel="stylesheet">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link
 		href="https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Dancing+Script&family=Dosis:wght@300&family=Fuggles&display=swap"
