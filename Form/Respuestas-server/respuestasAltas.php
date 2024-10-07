0b15191566569dd5b9712d6a69908d7e0bcf8716
NicoDelgadoHUB <ncklsdelgado@gmail.com>
2024-10-07 15:20:02 -0300

    Create respuestasAltas.php

---
+++ Form/Respuestas-server/respuestasAltas.php
@@ -0,0 +1,7 @@
+<div class="Rcontainer">
+    <div class="Rbox">
+        <h2 class="Rtitulo"></h2>
+        <h3 class="Rcuerpo">El Registro se guardo correctamente</h3>
+        <a href="../Form/Menu.php" class="cerrar">Cerrar</a>
+    </div>
+</div>
