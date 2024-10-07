0d307829836806777258106e1ee725d7cc4f5a3c
NicoDelgadoHUB <ncklsdelgado@gmail.com>
2024-10-07 15:28:31 -0300

    Update User.php

---
+++ Server/Peticiones/User.php
@@ -0,0 +1,9 @@
+<?php
+include '../../Conexion/Usuario.php';
+$json = json_decode(file_get_contents('php://input'), true);
+if ($json['solicitud']) {
+    $user = $json['solicitud'];
+    $respuesta = getUsuarioUserName($user);
+}
+echo json_encode($respuesta, true);
+?>
