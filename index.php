3e1a2709c9d7a30bf72028f6332b8b7642a44be9
Nico <ncklsdelgado@gmail.com>
2024-10-16 14:31:59 -0300

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <meta http-equiv="Pragma" content="no-cache">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Dancing+Script&display=swap" rel="stylesheet">
+    <link href="https://fonts.cdnfonts.com/css/star-wars" rel="stylesheet">
     <link rel="stylesheet" href="css/index.css">
 </head>
 <body>
